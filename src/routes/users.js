3cdd1391569beac6bb8855bc2ef4e5a036dd423a
Sparsh Jain <sparshjain2828@gmail.com>
2026-03-07 22:19:51 +0530

    Add route to get users by team leader ID

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,8 +7,10 @@ const router = express.Router();
 router.use(authenticate);
 
 router.get('/', authorize('admin', 'manager'), getAllUsers);
+router.get('/team/:leaderId', getUserById);
 router.get('/:id', getUserById);
 router.post('/', authorize('admin', 'manager'), createUser);
+router.put('/:id/role', authorize('admin', 'manager'), updateUser);
 router.put('/:id', authorize('admin'), updateUser);
 router.delete('/:id', authorize('admin'), deleteUser);
 

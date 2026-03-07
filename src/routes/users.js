e019aa4589c4ee1eafcb0025ce93f197b1367c2a
Sparsh Jain <sparshjain2828@gmail.com>
2026-03-08 01:35:47 +0530

    Add search and team member routes with authorization

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from '../controllers/userController.js';
+import { getAllUsers, getUserById, createUser, updateUser, deleteUser, searchUser, getTeamMembers } from '../controllers/userController.js';
 import { authenticate, authorize } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.use(authenticate);
 
-router.get('/', authorize('admin', 'manager'), getAllUsers);
-router.get('/team/:leaderId', getUserById);
+router.get('/', authorize('admin', 'manager', 'team_leader'), getAllUsers);
+router.get('/search', authorize('admin', 'manager', 'team_leader'), searchUser);
+router.get('/team/:leaderId', authorize('admin', 'manager', 'team_leader'), getTeamMembers);
 router.get('/:id', getUserById);
 router.post('/', authorize('admin', 'manager'), createUser);
 router.put('/:id/role', authorize('admin', 'manager'), updateUser);
